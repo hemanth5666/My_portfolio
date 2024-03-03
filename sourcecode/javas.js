273345cf0d9665f09ac1916c2a8719a185efd5bb
HEMANTH B <95521874+hemanth5666@users.noreply.github.com>
2024-03-03 10:34:06 +0530

    Update javas.js

--- sourcecode/javas.js
+++ sourcecode/javas.js
@@ -14,3 +14,31 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+// JavaScript Code
+// JavaScript Code
+// JavaScript Code
+let projectIndex = 0;
+const transitionDuration = 300; // Adjust this value as needed
+
+function showProjects() {
+    const projects = document.querySelectorAll('.project-box:not(.empty)');
+    const projectContainer = document.querySelector('.project-container');
+    
+    for (let i = 0; i < projects.length; i++) {
+        projects[i].style.transitionDuration = `${transitionDuration}ms`;
+        projects[i].style.transform = `translateX(-${projectIndex * 100}%)`;
+    }
+    
+    projectIndex++;
+    if (projectIndex >= projects.length - 2) { 
+        projectContainer.style.transitionDuration = '0ms';
+        projectIndex = 0; 
+        setTimeout(() => {
+            projectContainer.style.transitionDuration = `${transitionDuration}ms`;
+        }, 50); // Allow a short delay before re-starting the transition
+    }
+
+    setTimeout(showProjects, 2000); // Change slide every 2 seconds
+}
+
+showProjects();
